d17b8523012e6ff99706b4047e6ebbc7a8171d53
bulatsa <sadikovba@gmail.com>
2018-01-14 14:39:20 +0300

    Initial

---
+++ footer.php
@@ -0,0 +1,77 @@
+<!-- Модальные окна -->
+<div class="modals-sec">
+
+	<div id="modal-order-1" class="modal">
+		<p>Заказать обратный звонок</p>
+		<p>Оставьте заявку и наш администратор свяжется<br>с&nbsp;Вами в течение 10 минут</p>
+		<form class="ajax-form">
+			<input type="text" name="user_name" placeholder="Введите имя" data-label="Имя пользователя">
+			<input type="email" name="user_email" placeholder="Введите e-mail*" data-label="Email" data-req="true">
+			<input type="tel" name="user_tel" data-label="Телефон" placeholder="Введите телефон*" data-req="true">
+			<input type="hidden" value="Новая заявка" name="form_subject">
+			<button type="submit" class="btn">Отправить</button>
+			<a href="#" class="btn">Отправить</a>
+		</form>
+		<p>Оставьте заявку и наш администратор свяжется<br>с&nbsp;Вами в течение 10 минут</p>
+		<p>Оставьте заявку и наш администратор свяжется<br>с&nbsp;Вами в течение 10 минут</p>
+		<p>Оставьте заявку и наш администратор свяжется<br>с&nbsp;Вами в течение 10 минут</p>
+		<p>Оставьте заявку и наш администратор свяжется<br>с&nbsp;Вами в течение 10 минут</p>
+		<p>Оставьте заявку и наш администратор свяжется<br>с&nbsp;Вами в течение 10 минут</p>
+		<p>Оставьте заявку и наш администратор свяжется<br>с&nbsp;Вами в течение 10 минут</p>
+	</div>
+
+	<div id="modal-order-2" class="modal">
+		<p>Заказать обратный звонок</p>
+		<p>Оставьте заявку и наш администратор свяжется<br>с&nbsp;Вами в течение 10 минут</p>
+		<form class="ajax-form">
+			<input type="text" name="user_name" placeholder="Введите имя" data-label="Имя пользователя">
+			<input type="email" name="user_email" placeholder="Введите e-mail*" data-label="Email" data-req="true">
+			<input type="tel" name="user_tel" data-label="Телефон" placeholder="Введите телефон*" data-req="true">
+			<input type="hidden" value="Новая заявка" name="form_subject">
+			<button type="submit" class="btn">Отправить</button>
+			<a href="#" class="btn">Отправить</a>
+		</form>
+	</div>
+
+	<div id="modal-thanks" class="modal">
+		<p>Спасибо за заявку!</p>
+	</div>
+
+</div>
+<!-- Модальные окна -->
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<script src="js/assets.js" type="text/javascript" ></script>
+<script src="js/main.js" type="text/javascript" ></script>
+
+<!-- Yandex.Metrika counter -->
+<script type="text/javascript" >
+	(function (d, w, c) {
+		(w[c] = w[c] || []).push(function() {
+			try {
+				w.yaCounter47293344 = new Ya.Metrika({
+					id:47293344,
+					clickmap:true,
+					trackLinks:true,
+					accurateTrackBounce:true
+				});
+			} catch(e) { }
+		});
+
+		var n = d.getElementsByTagName("script")[0],
+			s = d.createElement("script"),
+			f = function () { n.parentNode.insertBefore(s, n); };
+		s.type = "text/javascript";
+		s.async = true;
+		s.src = "https://mc.yandex.ru/metrika/watch.js";
+
+		if (w.opera == "[object Opera]") {
+			d.addEventListener("DOMContentLoaded", f, false);
+		} else { f(); }
+	})(document, window, "yandex_metrika_callbacks");
+</script>
+<noscript><div><img src="https://mc.yandex.ru/watch/47293344" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+<!-- /Yandex.Metrika counter -->
+
+	</body>
+</html>
---
+++ index.php
@@ -0,0 +1,135 @@
+﻿<?php include('header.php'); ?>
+
+<section class="sidebar-sec">
+	<div class="sidebar">
+		<div class="sidebar__head">
+			<img src="img/man-logo.jpg" alt="Булат Садиков">
+			<p>Булат Садиков<br><span>веб-разработчик</span></p>
+			<div class="sidebar__head-descr">
+				<span>5 лет опыта разработки сайтов</span>
+				<span>40+ завершенных проектов</span>
+				<span>2 приза Лучший сайт</span>
+			</div>
+		</div>
+		<div class="sidebar__prices">
+			<p>Верстка одностраничного сайта -&nbsp;от&nbsp;15&nbsp;т.р.</p>
+			<p>Верстка сайта-портфолио -&nbsp;от&nbsp;25&nbsp;т.р.</p>
+			<p>Верстка корпоративного сайта -&nbsp;от&nbsp;50&nbsp;т.р.</p>
+		</div>
+		<div class="sidebar__bottom">
+			<p>Контакты</p>
+			<noindex>
+				<a href="tel:89274293569" rel="nofollow">+7 927 429 35 69</a>
+				<a href="mailto:sadikovba@gmail.com" rel="nofollow">sadikovba@gmail.com</a>
+			</noindex>
+			<div class="sidebar__menu-contacts">
+				<a href="https://vk.com/bulatkin" target="_blank" rel="nofollow"><img src="img/vk.svg" alt=""></a>
+				<a href="https://www.facebook.com/sadikovba" target="_blank" rel="nofollow"><img src="img/fb.svg" alt=""></a>
+				<a href="https://github.com/BulatSa" target="_blank" rel="nofollow"><img src="img/gb.svg" alt=""></a>
+			</div>
+
+		</div>
+	</div>
+</section>
+
+
+<section class="portfolio-sec">
+	<div class="portfolio">
+		<a href="http://mirtash.ru/" target="_blank" rel="nofollow" class="portfolio__item">
+			<img src="img/mirtash.jpg" alt="">
+			<p>
+				<span>Mirtash</span>
+				Верстка сайта
+			</p>
+		</a>
+		<a href="http://daob.ru/" class="portfolio__item" target="_blank" rel="nofollow">
+			<img src="img/nikita.jpg" alt="">
+			<p>
+				<span>Никита Флоринский</span>
+				Верстка и программирование на Bitrix
+			</p>
+		</a>
+		<a href="http://fin-1.com/finrestart" class="portfolio__item" target="_blank" rel="nofollow">
+			<img src="img/finperegruz.jpg" alt="">
+			<p>
+				<span>Финансовая перезагрузка</span>
+				Верстка сайта в команде
+			</p>
+		</a>
+		<a href="http://www.icl.ru/" class="portfolio__item" target="_blank" rel="nofollow">
+			<img src="img/icl.jpg" alt="">
+			<p>
+				<span>Компания ICL</span>
+				Верстка сайта
+			</p>
+		</a>
+
+		<a href="https://noskov.ru/" class="portfolio__item" target="_blank" rel="nofollow">
+			<img src="img/noskov.jpg" alt="">
+			<p>
+				<span>Игорь Носков</span>
+				Верстка сайта в команде
+			</p>
+		</a>
+		<a href="https://vremya-letat.ru/" class="portfolio__item" target="_blank" rel="nofollow">
+			<img src="img/vremya.jpg" alt="">
+			<p>
+				<span>Время летать</span>
+				Верстка сайта в команде
+			</p>
+		</a>
+
+		<a href="http://sdive.ru/" class="portfolio__item" target="_blank" rel="nofollow">
+			<img src="img/sdive.jpg" alt="">
+			<p>
+				<span>Sdive Казань</span>
+				Верстка сайта
+			</p>
+		</a>
+		<a href="http://finagrotrade24.ru/" class="portfolio__item" target="_blank" rel="nofollow">
+			<img src="img/finagro.jpg" alt="">
+			<p>
+				<span>ФинАгроТрейд</span>
+				Верстка сайта
+			</p>
+		</a>
+		<a href="http://archishow.ru/" class="portfolio__item" target="_blank" rel="nofollow">
+			<img src="img/archi.jpg" alt="">
+			<p>
+				<span>АрчиШоу</span>
+				Верстка сайта
+			</p>
+		</a>
+		<a href="http://mk.penbrain.ru/" class="portfolio__item" target="_blank" rel="nofollow">
+			<img src="img/pb.jpg" alt="">
+			<p>
+				<span>Marketing Kit | PenBrain</span>
+				Верстка сайта
+			</p>
+		</a>
+		<a href="http://r1akademiya.ru/" class="portfolio__item" target="_blank" rel="nofollow">
+			<img src="img/akademya.jpg" alt="">
+			<p>
+				<span>Академия единоборств</span>
+				Верстка и программирование на Bitrix
+			</p>
+		</a>
+		<a href="http://babykulinar.ru/" class="portfolio__item" target="_blank" rel="nofollow">
+			<img src="img/eva.jpg" alt="">
+			<p>
+				<span>Детская кулинарная студия Ева</span>
+				Верстка сайта
+			</p>
+		</a>
+		<a href="http://kidspacepark.ru/" class="portfolio__item" target="_blank" rel="nofollow">
+			<img src="img/kidspace.jpg" alt="">
+			<p>
+				<span>Кидспейс - детский город</span>
+				Верстка сайта
+			</p>
+		</a>
+	</div>
+</section>
+
+
+<?php include('footer.php'); ?>
